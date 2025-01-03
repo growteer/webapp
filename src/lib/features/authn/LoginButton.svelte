3b47cee93e111c5277789071b8aa196d33d9ec18
Christian Brennecke-Räther <c.b.raether@gmail.com>
2025-01-03 10:18:29 +0100

    refactor(auth): move api calls out of component script
    CU-8697696pu

--- src/lib/features/authn/LoginButton.svelte
+++ src/lib/features/authn/LoginButton.svelte
@@ -2,53 +2,34 @@
 	import { GradientButton } from 'flowbite-svelte';
 	import { getClient } from 'svelte-apollo';
 	import { initWeb3Auth } from '$lib/services/auth/web3auth';
-	import type { GenerateNonceMutation, LoginInput, LoginMutation, NonceInput } from '$lib/api/generated/types';
-	import { GENERATE_NONCE, LOGIN } from './mutations.gql';
+	import { generateNonce, login } from './mutations.gql';
 	import { EtherClient } from '$lib/services/ethereum/client';
 	import { setSessionToken } from '$lib/storage/local';
 
 	const gqlClient = getClient();
 
-	//TODO: cleanup, too much stuff going on within the component
 	const handleLogin = async () => {
 		const web3auth = await initWeb3Auth();
-		const provider = await web3auth.connect();
 
+		// Open the Web3Auth Modal
+		const provider = await web3auth.connect();
 		if (!provider) throw new Error('login failed');
 
+		// Get the Ethereum address
 		const ethClient = new EtherClient(provider);
-
 		const address = await ethClient.getFirstAccount();
-		const nonce = await generateNonce(address);
+
+		// Sign a message for verification
+		const nonce = await generateNonce(gqlClient, address);
 		const message = ethClient.newLoginMessage(nonce);
 		const signature = await ethClient.signLogin(address, message);
 
+		// Verify signature through the backend and get a token
+		const serializedSignature = Buffer.from(signature).toString('base64');
+		const sessionToken = await login(gqlClient, address, message, serializedSignature);
+
 		setSessionToken(sessionToken);
 		window.location.href = '/';
-	};
-
-	const generateNonce = async (address: string) => {
-		const { data, errors } = await gqlClient.mutate<GenerateNonceMutation, NonceInput>({
-			mutation: GENERATE_NONCE,
-			variables: { address }
-		});
-
-		if (errors?.length) throw new Error(errors[0].message);
-		if (!data || !data.generateNonce?.value) throw new Error('could not generate a nonce');
-
-		return data.generateNonce.value;
-	};
-
-	const login = async (address: string, message: string, signature: string) => {
-		const { data, errors } = await gqlClient.mutate<LoginMutation, LoginInput>({
-			mutation: LOGIN,
-			variables: { address, message, signature }
-		});
-
-		if (errors?.length) throw new Error(errors[0].message);
-		if (!data || !data.login.sessionToken) throw new Error('could not log in');
-
-		return data.login.sessionToken;
 	};
 </script>
 
--- src/lib/features/authn/mutations.gql.ts
+++ src/lib/features/authn/mutations.gql.ts
@@ -1,6 +1,7 @@
-import { gql } from '@apollo/client';
+import type { GenerateNonceMutation, LoginInput, LoginMutation, NonceInput } from '$lib/api/generated/types';
+import { ApolloClient, gql } from '@apollo/client';
 
-export const GENERATE_NONCE = gql`
+const GENERATE_NONCE = gql`
 	mutation GenerateNonce($address: String!) {
 		generateNonce(input: { address: $address }) {
 			value
@@ -8,10 +9,34 @@ export const GENERATE_NONCE = gql`
 	}
 `;
 
-export const LOGIN = gql`
+const LOGIN = gql`
 	mutation Login($address: String!, $message: String!, $signature: String!) {
 		login(input: { address: $address, message: $message, signature: $signature }) {
 			sessionToken
 		}
 	}
 `;
+
+export const generateNonce = async (client: ApolloClient<Cache>, address: string) => {
+	const { data, errors } = await client.mutate<GenerateNonceMutation, NonceInput>({
+		mutation: GENERATE_NONCE,
+		variables: { address }
+	});
+
+	if (errors?.length) throw new Error(errors[0].message);
+	if (!data || !data.generateNonce?.value) throw new Error('could not generate a nonce');
+
+	return data.generateNonce.value;
+};
+
+export const login = async (client: ApolloClient<Cache>, address: string, message: string, signature: string) => {
+	const { data, errors } = await client.mutate<LoginMutation, LoginInput>({
+		mutation: LOGIN,
+		variables: { address, message, signature }
+	});
+
+	if (errors?.length) throw new Error(errors[0].message);
+	if (!data || !data.login.sessionToken) throw new Error('could not log in');
+
+	return data.login.sessionToken;
+};
