dcab2826a64dc0ade6b8baad8b7c00648fc839dc
Christian Brennecke-Räther <c.b.raether@gmail.com>
2025-01-03 10:21:34 +0100

    feat(auth): inject bearer token to graphql requests
    CU-8697696pu

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,7 +1,25 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, from, InMemoryCache } from '@apollo/client';
 import { PUBLIC_API_URL } from '$env/static/public';
+import { getSessionToken } from '$lib/storage/local';
+
+const getHeaders = () => {
+	const sessionToken = getSessionToken();
+	return sessionToken ? { Authorization: `Bearer ${sessionToken}` } : {};
+};
+
+const auth = new ApolloLink((operation, next) => {
+	operation.setContext(({ headers = {} }) => ({
+		headers: {
+			...headers,
+			...getHeaders()
+		}
+	}));
+
+	return next(operation);
+});
 
 export const client = new ApolloClient({
 	uri: PUBLIC_API_URL,
-	cache: new InMemoryCache()
+	cache: new InMemoryCache(),
+	link: from([auth])
 });
