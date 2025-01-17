3db042e4758ca94d81c334aaf55851389022d38f
Christian Brennecke-Räther <c.b.raether@gmail.com>
2025-01-18 00:22:49 +0100

    feat: forward to signup logged in user has not signed up

--- src/lib/features/authn/LoginButton.svelte
+++ src/lib/features/authn/LoginButton.svelte
@@ -2,9 +2,12 @@
 	import { initWeb3Auth } from '$lib/services/auth/web3auth';
 	import { generateNonce, login } from './mutations.gql';
 	import { SolanaClient } from '$lib/services/solana/client';
-	import { setRefreshToken, setSessionToken } from '$lib/storage/local';
 	import { LogIn } from 'lucide-svelte';
-	import { Navigation } from '@skeletonlabs/skeleton-svelte';
+	import { Navigation, type ToastContext } from '@skeletonlabs/skeleton-svelte';
+	import { getContext } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	export const toast: ToastContext = getContext<ToastContext>('toast');
 
 	const handleLogin = async () => {
 		const web3auth = await initWeb3Auth();
@@ -13,21 +16,34 @@
 		const provider = await web3auth.connect();
 		if (!provider) throw new Error('login failed');
 
-		// Get the wallet address
-		const client = new SolanaClient(provider);
-		const address = await client.getAddress();
+		try {
+			// Get the wallet address
+			const client = new SolanaClient(provider);
+			const address = await client.getAddress();
 
-		// Sign a message for verification
-		const nonce = await generateNonce(address);
-		const { message, signature } = await client.signLogin(nonce);
+			// Sign a message for verification
+			const nonce = await generateNonce(address);
+			const { message, signature } = await client.signLogin(nonce);
 
-		// Verify signature through the backend and get a token
-		const { sessionToken, refreshToken } = await login(address, message, signature);
+			// Verify signature through the backend and set tokens in local storage
+			const loggedIn = await login(address, message, signature);
+			if (!loggedIn) return goto('/signup');
+		} catch (err) {
+			toast.create({
+				description: String(err),
+				type: 'error'
+			});
 
-		setSessionToken(sessionToken);
-		setRefreshToken(refreshToken);
+			await web3auth.logout();
+			return goto('/');
+		}
 
-		window.location.href = '/';
+		toast.create({
+			description: 'Successfully logged in',
+			type: 'success'
+		});
+
+		goto('/');
 	};
 </script>
 
