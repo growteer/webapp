3ae23ce1acd96d0b80f298480ff6730cf94fb3b4
Christian Brennecke-Räther <c.b.raether@gmail.com>
2025-01-20 21:49:41 +0100

    feat: add heading & input component

---
+++ src/lib/components/heading/heading.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	interface Props {
+		type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+		children?: import('svelte').Snippet;
+	}
+
+	let { type, children }: Props = $props();
+	let bottomMargin = $derived.by(() => {
+		switch (type) {
+			case 'h1':
+				return 'mb-8';
+			case 'h2':
+				return 'mb-6';
+			case 'h3':
+				return 'mb-4';
+			default:
+				return 'mb-2';
+		}
+	});
+</script>
+
+<svelte:element this={type} class="{type} {bottomMargin}">
+	{@render children?.()}
+</svelte:element>
