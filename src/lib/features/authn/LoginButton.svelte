a7bfac09b1ce5d88e129b0af5ddc45506ed0b9b3
Christian Brennecke-Räther <c.b.raether@gmail.com>
2025-01-20 21:46:48 +0100

    refactor: move login/logout logic from components to service

--- src/lib/features/authn/LoginButton.svelte
+++ src/lib/features/authn/LoginButton.svelte
@@ -1,32 +1,16 @@
 <script lang="ts">
-	import { initWeb3Auth } from '$lib/services/auth/web3auth';
-	import { generateNonce, login } from './mutations.gql';
-	import { SolanaClient } from '$lib/services/solana/client';
+	import { AuthClient } from '$lib/services/authn/client';
 	import { LogIn } from 'lucide-svelte';
 	import { Navigation, type ToastContext } from '@skeletonlabs/skeleton-svelte';
 	import { getContext } from 'svelte';
 	import { goto } from '$app/navigation';
 
 	export const toast: ToastContext = getContext<ToastContext>('toast');
+	const auth = new AuthClient();
 
 	const handleLogin = async () => {
-		const web3auth = await initWeb3Auth();
-
-		// Open the Web3Auth Modal
-		const provider = await web3auth.connect();
-		if (!provider) throw new Error('login failed');
-
 		try {
-			// Get the wallet address
-			const client = new SolanaClient(provider);
-			const address = await client.getAddress();
-
-			// Sign a message for verification
-			const nonce = await generateNonce(address);
-			const { message, signature } = await client.signLogin(nonce);
-
-			// Verify signature through the backend and set tokens in local storage
-			const loggedIn = await login(address, message, signature);
+			const loggedIn = await auth.login();
 			if (!loggedIn) return goto('/signup');
 		} catch (err) {
 			toast.create({
@@ -34,7 +18,6 @@
 				type: 'error'
 			});
 
-			await web3auth.logout();
 			return goto('/');
 		}
 
---
+++ src/lib/services/authn/client.ts
@@ -0,0 +1,37 @@
+import { removeRefreshToken, removeSessionToken } from '$lib/storage/local';
+import { SolanaClient } from '../solana/client';
+import { web3Auth } from '../w3a/web3auth';
+import { generateNonce, login } from './mutations.gql';
+
+export class AuthClient {
+	async login(): Promise<boolean> {
+		if (!web3Auth) throw new Error('cannot create AuthClient without web3auth being initialized');
+
+		// Open the Web3Auth Modal
+		const provider = await web3Auth.connect();
+		if (!provider) throw new Error('login failed');
+
+		try {
+			// Get the wallet address
+			const client = new SolanaClient(provider);
+			const address = await client.getAddress();
+
+			// Sign a message for verification
+			const nonce = await generateNonce(address);
+			const { message, signature } = await client.signLogin(nonce);
+
+			// Verify signature through the backend and set tokens in local storage
+			const loggedIn = await login(address, message, signature);
+			return loggedIn;
+		} catch (err) {
+			await web3Auth.logout();
+			throw err;
+		}
+	}
+
+	async logout() {
+		removeSessionToken();
+		removeRefreshToken();
+		return web3Auth.logout();
+	}
+}
