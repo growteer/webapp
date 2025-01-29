111b1ab21b65a3f0c5b5c9f0c16bd59ff19a872a
Christian Brennecke-Räther <c.b.raether@gmail.com>
2025-01-29 21:40:16 +0100

    feat: add profile form

---
+++ src/lib/features/profile/schema.ts
@@ -0,0 +1,12 @@
+export interface FormData {
+	firstname: string;
+	lastname: string;
+	dateOfBirth: string;
+	primaryEmail: string;
+	country: string;
+	postalCode?: string;
+	city?: string;
+	website?: string;
+	personalGoal?: string;
+	about?: string;
+}
