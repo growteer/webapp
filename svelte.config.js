760678beb33231492939f75403676caa285a1eeb
Christian Brennecke-Räther <c.b.raether@gmail.com>
2025-08-15 08:14:39 +0200

    chore: update dependencies and fix configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter({ strict: false }),
-		alias: {
-			$src: '.src',
-			$lib: './src/lib',
-			$features: './src/lib/features',
-			$services: './src/lib/services'
-		}
-	}
+	kit: { adapter: adapter({ strict: false }) }
 };
 
 export default config;
--- vitest-setup-client.ts
+++ vitest-setup-client.ts
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/vitest';
-import { beforeAll, afterEach, afterAll, vi } from 'vitest';
+import { beforeAll, afterEach, afterAll } from 'vitest';
 import { server } from './src/mocks/server';
 
 beforeAll(() => server.listen());
@@ -7,17 +7,3 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 
 afterAll(() => server.close());
-
-// required for svelte5 + jsdom as jsdom does not support matchMedia
-Object.defineProperty(window, 'matchMedia', {
-	writable: true,
-	enumerable: true,
-	value: vi.fn().mockImplementation((query) => ({
-		matches: false,
-		media: query,
-		onchange: null,
-		addEventListener: vi.fn(),
-		removeEventListener: vi.fn(),
-		dispatchEvent: vi.fn()
-	}))
-});
