6d2ad6f61485081ca929819dca927239ceafd25f
Christian Brennecke-Räther <c.b.raether@gmail.com>
2025-08-24 16:00:39 +0200

    fix: profileform test occasionally failing due to timing

--- src/mocks/profile.handlers.ts
+++ src/mocks/profile.handlers.ts
@@ -1,7 +1,13 @@
+import type { Profile, UpdateUserProfileMutationVariables } from '$lib/api/generated/types';
 import { graphql, HttpResponse } from 'msw';
 
 export const profileHandlers = [
-	graphql.mutation('UpdateUserProfile', () => {
+	graphql.mutation<Profile, UpdateUserProfileMutationVariables>('UpdateUserProfile', async ({ variables }) => {
+		if (variables.profile.firstName === 'delayed') {
+			await new Promise((resolve) => setTimeout(resolve, 200));
+			return HttpResponse.json({});
+		}
+
 		return HttpResponse.json({});
 	})
 ];
